dd80bc8b0b64d4f6ef77fd5f66369ca2da9b34dc
FuminocchiDesu <furuhashi0fumino@gmail.com>
2024-11-04 11:47:40 +0000

    asd

--- kohi/kohi/settings.py
+++ kohi/kohi/settings.py
@@ -186,5 +186,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
